27fd17c270dae07bbfb696c318be88630a3c12a7
arunv <me@arunv.com>
2014-08-04 14:40:37 -0400

    Image style

---
+++ ExpandTableView/JKExpandTableViewImageStyle.h
@@ -0,0 +1,15 @@
+//
+//  JKExpandTableViewImageStyle.h
+//  Fabric
+//
+//  Created by Arun Vijayvergiya on 8/4/14.
+//  Copyright (c) 2014 StoryTellers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, JKExpandTableViewCellImageStyle) {
+    JKExpandTableViewCellImageStyleSquare,
+    JKExpandTableViewCellImageStyleCircle,
+    JKExpandTableViewCellImageStyleRoundedRect,
+};
